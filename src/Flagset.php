f1af745b996e0afb9b99a78c93b01a54011a8a91
Максим <67472512+QUAD69@users.noreply.github.com>
2021-09-24 00:38:47 +0300

    Add files via upload

--- src/Flagset.php
+++ src/Flagset.php
@@ -1,9 +1,22 @@
 <?php
+declare(strict_types=1);
 
 namespace Quad69;
 
+/**
+ * Класс для работы с большим набором флагов (true/false) и хранения
+ * их в компактном (бинарном) виде. Также есть методы для объединения
+ * или исключения одной группы флагов из другой.
+ *
+ * Тип колонки для MySQL - BINARY(255)
+ *
+ * @author QUAD69 <https://vk.com/quad69>
+ */
 class Flagset implements \Stringable
 {
+    /**
+     * Фиксированная длина строки для флагов.
+     */
     public const LENGTH = 255;
 
     private string $content;
@@ -13,6 +26,13 @@ class Flagset implements \Stringable
         $this->content = str_pad($content, self::LENGTH, "\0");
     }
 
+    /**
+     * Задает значение указанному флагу
+     *
+     * @param int $num Номер флага (от 0 до 255) который необходимо изменить
+     * @param bool $value Значение true/false для флага
+     * @return static
+     */
     public function set(int $num, bool $value): static
     {
         if ($num >= 0 and $num < self::LENGTH)
@@ -30,6 +50,12 @@ class Flagset implements \Stringable
         return $this;
     }
 
+    /**
+     * Проверяет значение указанного флага
+     *
+     * @param int $num Номер флага (от 0 до 255) который необходимо проверить
+     * @return bool Значение флага (true/false)
+     */
     public function has(int $num): bool
     {
         $byte = intdiv($num, 8);
@@ -37,29 +63,51 @@ class Flagset implements \Stringable
 
         return (ord($this->content[$byte]) & $bit) > 0;
     }
-
+    
+    /**
+     * Добавить флаги из другого объекта Flagset или строки (побитовое ИЛИ)
+     *
+     * @param string $flagset Flagset или строка для объединения
+     * @return static
+     */
     public function append(string $flagset): static
     {
         $this->content |= $flagset; return $this;
     }
-
+    
+    /**
+     * Вычесть флаги из другого объекта Flagset или строки (побитовое И НЕ)
+     *
+     * @param string $flagset Flagset или строка для вычетания
+     * @return static
+     */
     public function subtract(string $flagset): static
     {
         $this->content &= ~$flagset; return $this;
     }
 
+    /**
+     * Получить FlagSet где все флаги заданы в значение false
+     *
+     * @return static
+     */
+    public static function getNulled(): static
+    {
+        return new static(str_repeat("\0", self::LENGTH));
+    }
+    
+    /**
+     * Получить FlagSet где все флаги заданы в значение true
+     *
+     * @return static
+     */
+    public static function getFilled(): static
+    {
+        return new static(str_repeat("\xff", self::LENGTH));
+    }
+    
     public function __toString()
     {
         return $this->content;
-    }
-
-    public static function getNulled(): string
-    {
-        return str_repeat("\0", self::LENGTH);
-    }
-
-    public static function getFilled(): string
-    {
-        return str_repeat("\xff", self::LENGTH);
     }
 }
